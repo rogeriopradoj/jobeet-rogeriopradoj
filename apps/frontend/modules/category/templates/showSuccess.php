a9cb4fa93080635c5c301cca2e355fbe0fcac88b
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2011-04-05 14:44:19 -0300

    Day 8: The Unit Tests

--- apps/frontend/modules/category/templates/showSuccess.php
+++ apps/frontend/modules/category/templates/showSuccess.php
@@ -15,11 +15,11 @@
 <?php if ($pager->haveToPaginate()): ?>
   <div class="pagination">
     <a href="<?php echo url_for('category', $category) ?>?page=1">
-      <img src="/images/first.png" alt="First page" title="First page" />
+      <img src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/first.png" alt="First page" title="First page" />
     </a>
 
     <a href="<?php echo url_for('category', $category) ?>?page=<?php echo $pager->getPreviousPage() ?>">
-      <img src="/images/previous.png" alt="Previous page" title="Previous page" />
+      <img src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/previous.png" alt="Previous page" title="Previous page" />
     </a>
 
     <?php foreach ($pager->getLinks() as $page): ?>
@@ -31,11 +31,11 @@
     <?php endforeach; ?>
 
     <a href="<?php echo url_for('category', $category) ?>?page=<?php echo $pager->getNextPage() ?>">
-      <img src="/images/next.png" alt="Next page" title="Next page" />
+      <img src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/next.png" alt="Next page" title="Next page" />
     </a>
 
     <a href="<?php echo url_for('category', $category) ?>?page=<?php echo $pager->getLastPage() ?>">
-      <img src="/images/last.png" alt="Last page" title="Last page" />
+      <img src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/last.png" alt="Last page" title="Last page" />
     </a>
   </div>
 <?php endif; ?>
--- lib/Jobeet.class.php
+++ lib/Jobeet.class.php
@@ -2,14 +2,32 @@
 // lib/Jobeet.class.php
 class Jobeet
 {
-  static public function slugify($text)
-  {
-    // replace all non letters or digits by -
-    $text = preg_replace('/\W+/', '-', $text);
+    // code derived from http://php.vrana.cz/vytvoreni-pratelskeho-url.php
+    static public function slugify($text)
+    {
+      // replace non letter or digits by -
+      $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
 
-    // trim and lowercase
-    $text = strtolower(trim($text, '-'));
+      // trim
+      $text = trim($text, '-');
 
-    return $text;
-  }
+      // transliterate
+      if (function_exists('iconv'))
+      {
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+      }
+
+      // lowercase
+      $text = strtolower($text);
+
+      // remove unwanted characters
+      $text = preg_replace('#[^-\w]+#', '', $text);
+
+      if (empty($text))
+      {
+        return 'n-a';
+      }
+
+      return $text;
+    }
 }
