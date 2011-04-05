adf43cbdf400182f9a55c9316b1f7aa5f5a139be
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2011-04-04 22:21:13 -0300

    Day 6: More with the Model

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -1,18 +1,33 @@
 <!-- apps/frontend/modules/job/templates/indexSuccess.php -->
 <?php use_stylesheet('jobs.css') ?>
 
+<?php use_stylesheet('jobs.css') ?>
+
 <div id="jobs">
-  <table class="jobs">
-    <?php foreach ($jobeet_jobs as $i => $job): ?>
-      <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
-        <td class="location"><?php echo $job->getLocation() ?></td>
-        <td class="position">
-          <a href="<?php echo url_for('job_show_user', $job) ?>">
-            <?php echo $job->getPosition() ?>
-          </a>
-        </td>
-        <td class="company"><?php echo $job->getCompany() ?></td>
-      </tr>
-    <?php endforeach ?>
-  </table>
+  <?php foreach ($categories as $category): ?>
+    <div class="category_<?php echo Jobeet::slugify($category->getName()) ?>">
+      <div class="category">
+        <div class="feed">
+          <a href="">Feed</a>
+        </div>
+        <h1><?php echo $category ?></h1>
+      </div>
+
+      <table class="jobs">
+       <?php foreach ($category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')) as $i => $job): ?>
+          <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
+            <td class="location">
+              <?php echo $job->getLocation() ?>
+            </td>
+            <td class="position">
+              <?php echo link_to($job->getPosition(), 'job_show_user', $job) ?>
+            </td>
+            <td class="company">
+              <?php echo $job->getCompany() ?>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    </div>
+  <?php endforeach; ?>
 </div>
--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -27,4 +27,15 @@ class JobeetJob extends BaseJobeetJob
     {
       return Jobeet::slugify($this->getLocation());
     }
+
+    public function save(Doctrine_Connection $conn = null)
+    {
+        if ($this->isNew() && !$this->getExpiresAt())
+        {
+          $now = $this->getCreatedAt() ? $this->getDateTimeObject('created_at')->format('U') : time();
+          $this->setExpiresAt(date('Y-m-d H:i:s', $now + 86400 * sfConfig::get('app_active_days')));
+        }
+
+        return parent::save($conn);
+    }
 }
